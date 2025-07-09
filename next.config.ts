40018b4504179413e3503cbde2f63f5277a67cc6
wag1twat <53095612+wag1twat@users.noreply.github.com>
2025-07-09 15:21:21 +0300

    fix/env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST || `https://${process.env.VERCEL_URL}`,
+    },
 };
 
 export default nextConfig;
