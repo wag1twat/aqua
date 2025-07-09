437604bad22751f1c9a0c94c114e153c9c164b61
wag1twat <53095612+wag1twat@users.noreply.github.com>
2025-07-09 15:29:18 +0300

    fix/build

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import { Client } from '@/client/client';
+import ClientDinamicSSR from '@/client/ClientDynamicSSR';
 
 export default async function Page() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/db`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/db`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    const db = await response.json();
-
-    return <Client data={db} />;
+        const db = await response.json();
+        return <ClientDinamicSSR data={db} />;
+    } catch {
+        return null;
+    }
 }
